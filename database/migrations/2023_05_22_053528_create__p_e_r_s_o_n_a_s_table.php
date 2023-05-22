1f03d1bba344750d14e40956281ae2e35938dc9b
Jesus Machaca <118281223+JsasMachaca@users.noreply.github.com>
2023-05-22 00:44:55 -0500

    Add files via upload

---
+++ database/migrations/2023_05_22_053528_create__p_e_r_s_o_n_a_s_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('_p_e_r_s_o_n_a_s', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_p_e_r_s_o_n_a_s');
+    }
+};
